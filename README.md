9c6ebb8c144349657af4a24ffab5006b87d2d3dd
Nick Sjostrom <nick@sjostrom.io>
2024-04-10 12:40:53 -0400

    initial

---
+++ README.md
@@ -0,0 +1,59 @@
+# github-deps
+
+List all dependencies of a GitHub repository, outputs to markdown file
+
+## Getting Started
+
+Pull the repository and install the dependencies:
+
+```bash
+# setup virtual environment
+python3 -m venv venv
+
+# activate virtual environment
+source venv/bin/activate
+
+# install dependencies
+pip install -r requirements.txt
+
+# create .env file with GITHUB_API_KEY
+echo "GITHUB_API_KEY=your_github_api_key" > .env
+```
+
+## Usage
+
+1. Update `config.json` with the organization and list of repos you want to list dependencies for.
+
+   ```json
+   {
+     "organization": "your_organization",
+     "repos": ["repo1", "repo2"]
+   }
+   ```
+
+   > NOTE: Be sure the repos you want to list dependencies for are public or you have access to them and they have the [dependency graph enabled](https://docs.github.com/en/code-security/supply-chain-security/understanding-your-software-supply-chain/configuring-the-dependency-graph)
+
+2. Run the script
+
+   ```bash
+   # run the script
+   python script.py
+   ```
+
+3. View the output
+
+   View the output in `deps.md` as a markdown formatted table.
+
+   **Sample output for this repository:**
+   | Package | Github Link | Version | Org Repos |
+   |-----|-----|-----|-----|
+   | certifi | [certifi/python-certifi](https://github.com/certifi/python-certifi) | `= 2024.2.2` | `github-deps` |
+   | charset-normalizer | [Ousret/charset_normalizer](https://github.com/Ousret/charset_normalizer) | `= 3.3.2` | `github-deps` |
+   | diskcache | [Baughn/python-diskcache](https://github.com/Baughn/python-diskcache) | `= 5.6.3` | `github-deps` |
+   | idna | [kjd/idna](https://github.com/kjd/idna) | `= 3.6` | `github-deps` |
+   | python-dotenv | [theskumar/python-dotenv](https://github.com/theskumar/python-dotenv) | `= 1.0.1` | `github-deps` |
+   | pyyaml | [yaml/pyyaml](https://github.com/yaml/pyyaml) | `= 6.0.1` | `github-deps` |
+   | requests | [psf/requests](https://github.com/psf/requests) | `= 2.31.0` | `github-deps` |
+   | tomark | [codazoda/tomark](https://github.com/codazoda/tomark) | `= 0.1.4` | `github-deps` |
+   | urllib3 | [urllib3/urllib3](https://github.com/urllib3/urllib3) | `= 2.2.1` | `github-deps` |
+   | xkcd2347 | [edsu/xkcd2347](https://github.com/edsu/xkcd2347) | `= 0.0.4` | `github-deps` |
---
+++ script.py
@@ -0,0 +1,78 @@
+import xkcd2347
+import json
+from tomark import Tomark
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Get GitHub API key
+GITHUB_API_KEY = os.getenv('GITHUB_API_KEY')
+
+# Exit if no key is found
+if GITHUB_API_KEY is None:
+   print("No GitHub API key found. Please add it to your .env file")
+   exit()
+
+# Create GitHub object
+gh = xkcd2347.GitHub(key=GITHUB_API_KEY)
+
+# Load config file
+f = open('config.json')
+data = json.load(f)
+
+# Get config values
+REPOS = data['repos']
+ORGANIZATION = data['organization']
+dependencies = []
+dependencyMap = {}
+
+# Get dependencies for each repo
+print(f"Getting dependencies for {len(REPOS)} repos of {ORGANIZATION}")
+for repo in REPOS:
+   print(f"Getting dependencies for {repo}...")
+   for dep in gh.get_dependencies(ORGANIZATION, repo):
+      # Get the nameWithOwner of the repo if it exists
+      nameWithOwner = "No git repo found"
+      repository = dep.get('repository', None)
+      if repository is not None:
+         nameWithOwner = repository.get('nameWithOwner', None)
+      
+      # Add to map if not already there
+      if nameWithOwner not in dependencyMap:
+         dependencyMap[nameWithOwner] = {
+               'package': dep['packageName'],
+               'github': f'[{nameWithOwner}](https://github.com/{nameWithOwner})',
+               'version': dep['requirements'],
+               'repos': []
+            }
+      
+      # If not already added to repos
+      if repo not in dependencyMap[nameWithOwner]['repos']:
+         dependencyMap[nameWithOwner]['repos'].append(repo)
+
+# Create a list of dependencies
+for key in dependencyMap:
+   # Turn `repos` into a string. surround the repo names with backticks
+   repos = dependencyMap[key]['repos']
+   reposStr = ', '.join([f'`{r}`' for r in repos])
+
+   # Add to list
+   dependencies.append({
+         'Package': dependencyMap[key]['package'],
+         'Github Link': dependencyMap[key]['github'],
+         'Version': f"`{dependencyMap[key]['version']}`",
+         'Org Repos': reposStr
+      })
+
+# Sort dependencies by package name
+sortedDeps = sorted(dependencies, key=lambda d: d['Package'])
+
+# Create markdown table
+markdown = Tomark.table(sortedDeps)
+
+# Write markdown to file
+f = open("deps.md", "w")
+f.write(markdown)
+f.close()
